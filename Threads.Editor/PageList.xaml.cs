c93b335b8d16ad000d122445be5c446dca40be18
Ian Burgmyer <spectere@gmail.com>
2016-08-12 15:59:11 -0400

    Began implementing PageList control.

---
+++ Threads.Editor/PageList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Threads.Editor {
+    /// <summary>
+    /// Interaction logic for PageList.xaml
+    /// </summary>
+    public partial class PageList {
+        public PageList() {
+            InitializeComponent();
+        }
+    }
+}
