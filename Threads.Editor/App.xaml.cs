735389ea6fe9f5300f6d47117db4bdb98086b417
Ian Burgmyer <spectere@gmail.com>
2016-08-12 11:15:29 -0400

    Recreated Threads.Editor as a WPF project; removed unused (and misspelled) test project.

---
+++ Threads.Editor/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Threads.Editor {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+    }
+}
---
+++ Threads.Editor/Storyboard.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Threads.Editor {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Storyboard {
+        public Storyboard() {
+            InitializeComponent();
+        }
+    }
+}
