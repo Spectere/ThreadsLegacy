0910d7bc486fb049e4e6ca0a6196ad35f91bb9d5
Ian Burgmyer <spectere@gmail.com>
2016-08-12 15:47:38 -0400

    Added an unimplemented menu to the editor window.

--- Threads.Editor/EditorWindow.xaml.cs
+++ Threads.Editor/EditorWindow.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace Threads.Editor {
+﻿using System.Windows;
+
+namespace Threads.Editor {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class EditorWindow {
         public EditorWindow() {
             InitializeComponent();
+        }
+
+        private void New_OnClick(object sender, RoutedEventArgs e) {
+            throw new System.NotImplementedException();
+        }
+
+        private void Open_OnClick(object sender, RoutedEventArgs e) {
+            throw new System.NotImplementedException();
+        }
+
+        private void Save_OnClick(object sender, RoutedEventArgs e) {
+            throw new System.NotImplementedException();
+        }
+
+        private void SaveAs_OnClick(object sender, RoutedEventArgs e) {
+            throw new System.NotImplementedException();
         }
     }
 }
