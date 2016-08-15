91b17ae61baed0d07f814009a2e92de9486c52d5
Ian Burgmyer <spectere@gmail.com>
2016-08-15 16:30:18 -0400

    Added event handlers for add/delete button on page/object list controls.

--- Threads.Editor/ObjectList.xaml.cs
+++ Threads.Editor/ObjectList.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace Threads.Editor {
+﻿using System.Windows;
+
+namespace Threads.Editor {
     /// <summary>
     /// Interaction logic for PageObjectList.xaml
     /// </summary>
     public partial class ObjectList {
+        public delegate void OnUpdate(object sender, RoutedEventArgs e);
+
+        /// <summary>
+        /// Fired when an item is added to the Object list.
+        /// </summary>
+        public event OnUpdate Add;
+
+        /// <summary>
+        /// Fired when an item is deleted from the Object list.
+        /// </summary>
+        public event OnUpdate Delete;
+
         public ObjectList() {
             InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e) {
+            Add?.Invoke(sender, e);
+        }
+
+        private void DelButton_OnClick(object sender, RoutedEventArgs e) {
+            Delete?.Invoke(sender, e);
         }
     }
 }
--- Threads.Editor/PageList.xaml.cs
+++ Threads.Editor/PageList.xaml.cs
@@ -1,12 +1,33 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Threads.Editor {
     /// <summary>
     /// Interaction logic for PageList.xaml
     /// </summary>
     public partial class PageList {
+        public delegate void OnUpdate(object sender, RoutedEventArgs e);
+
+        /// <summary>
+        /// Fired when an item is added to the Page list.
+        /// </summary>
+        public event OnUpdate Add;
+
+        /// <summary>
+        /// Fired when an item is deleted from the Page list.
+        /// </summary>
+        public event OnUpdate Delete;
+
         public PageList() {
             InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e) {
+            Add?.Invoke(sender, e);
+        }
+
+        private void DelButton_Click(object sender, RoutedEventArgs e) {
+            Delete?.Invoke(sender, e);
         }
     }
 }
