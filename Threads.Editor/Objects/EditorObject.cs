7c2a749f0a230bfa00263be2f9c63d0828a06f46
Ian Burgmyer <spectere@gmail.com>
2016-08-16 15:17:07 -0400

    Added base EditorObject.

---
+++ Threads.Editor/Objects/EditorObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Threads.Editor.Objects {
+    /// <summary>
+    /// The base class that all editor objects are derived from. Editor objects implement the UI and logic necessary to modify story objects.
+    /// </summary>
+    internal abstract class EditorObject : UserControl {
+        /// <summary>
+        /// The <see cref="StackPanel" /> that this <see cref="EditorObject" /> displays its controls in.
+        /// </summary>
+        protected StackPanel DesignerPanel;
+
+        /// <summary>
+        /// The friendly name of the object.
+        /// </summary>
+        public abstract string ObjectName { get; }
+
+        /// <summary>
+        /// A description of the object's functionality.
+        /// </summary>
+        public abstract string Description { get; }
+
+        /// <summary>
+        /// The <see cref="Type" /> of story object that this <see cref="EditorObject" /> handles.
+        /// </summary>
+        public abstract Type HandledType { get; }
+
+        /// <summary>
+        /// Displays the object editor in a <see cref="ContentControl" />.
+        /// </summary>
+        /// <param name="parent">The parent <see cref="ContentControl" /> that the editor controls should be displayed in.</param>
+        public void DisplayControls(ContentControl parent) {
+            parent.Content = GetDesignerUI();
+        }
+
+        /// <summary>
+        /// Displays the object editor in a <see cref="Panel" />.
+        /// </summary>
+        /// <param name="parent">The parent <see cref="Panel" /> that the editor controls should be displayed in.</param>
+        public void DisplayControls(Panel parent) {
+            parent.Children.Add(GetDesignerUI());
+        }
+        
+        /// <summary>
+        /// Returns the designer UI
+        /// </summary>
+        /// <returns></returns>
+        public abstract StackPanel GetDesignerUI();
+    }
+}
