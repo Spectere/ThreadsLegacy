373744ae75ec140179797c8b36b99d9a61cb0cc0
Ian Burgmyer <spectere@gmail.com>
2016-08-25 16:16:36 -0400

    Image support added to the editor; xmldocs.

--- Threads.Editor/Objects/EditorObjectFactory.cs
+++ Threads.Editor/Objects/EditorObjectFactory.cs
@@ -2,11 +2,20 @@
 using EngineObjects = Threads.Interpreter.Objects;
 
 namespace Threads.Editor.Objects {
+    /// <summary>
+    /// Includes a method to return an appropriate <see cref="EditorObject" /> when a <see cref="PageObject" /> is passed to it.
+    /// </summary>
     internal static class EditorObjectFactory {
+        /// <summary>
+        /// Returns the appropriate <see cref="EditorObject" /> for the <see cref="PageObject" /> that the method receives.
+        /// </summary>
+        /// <param name="pageObject">The <see cref="PageObject" /> to associate with the created <see cref="EditorObject" />.</param>
+        /// <returns>A specific <see cref="EditorObject" /> that is appropriate for the passed <see cref="PageObject" />. If no suitable object exists, null is returned.</returns>
         public static EditorObject Get(EngineObjects.Page.PageObject pageObject) {
-            if(pageObject.GetType() == typeof(EngineObjects.Page.Paragraph)) {
+            if(pageObject.GetType() == typeof(EngineObjects.Page.Paragraph))
                 return new Paragraph(pageObject);
-            }
+            if(pageObject.GetType() == typeof(EngineObjects.Page.Image))
+                return new Image(pageObject);
 
             return null;
         }
---
+++ Threads.Editor/Objects/Page/Image.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+using ThreadsImage = Threads.Interpreter.Objects.Page.Image;
+
+namespace Threads.Editor.Objects.Page {
+    internal class Image : PageObject {
+        public new static string ObjectName => "Image";
+        public new static string Description => "An object that displays images.";
+        public override Type HandledType => typeof(ThreadsImage);
+        public Image(Interpreter.Objects.Page.PageObject objectData) : base(objectData) {}
+
+        protected override void BuildPageObjectEditor() {
+            AppendRow(new Label { Content = "Path" },
+                CreateBoundTextBox(ObjectData, "Source"));
+
+            base.BuildPageObjectEditor();
+        }
+    }
+}
