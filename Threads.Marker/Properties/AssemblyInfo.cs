09ff24827f250d3bd9b38d393dca57b8915e10f0
Ian Burgmyer <spectere@gmail.com>
2016-08-31 09:56:03 -0400

    Updated assembly info.

--- Threads.Marker/Properties/AssemblyInfo.cs
+++ Threads.Marker/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Threads.Marker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Marker")]
+[assembly: AssemblyDescription("Interprets text blocks that are marked up in Threads Marker syntax.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SpectereSoft")]
 [assembly: AssemblyProduct("Threads.Marker")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©2016 Ian Burgmyer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
