e1afe1d25a24968a287df41da96bec9002934486
Ian Burgmyer <spectere@gmail.com>
2016-09-07 10:12:38 -0400

    Tabbing between TextBox objects now selects all text like in other Windows applications.

--- Threads.Editor/App.xaml.cs
+++ Threads.Editor/App.xaml.cs
@@ -1,7 +1,17 @@
-﻿namespace Threads.Editor {
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Threads.Editor {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App {
+        private void App_OnStartup(object sender, StartupEventArgs e) {
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.GotFocusEvent, new RoutedEventHandler(TextBox_GotFocus));
+        }
+
+        private static void TextBox_GotFocus(object sender, RoutedEventArgs e) {
+            (sender as TextBox)?.SelectAll();
+        }
     }
 }
