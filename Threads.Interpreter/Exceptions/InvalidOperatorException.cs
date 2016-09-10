33d3f930c7f35c3a79871d172b64093ac38abb5c
Ian Burgmyer <spectere@gmail.com>
2016-09-09 22:57:24 -0400

    Added some quick and very dirty expression support, and a sample file for it.

---
+++ Threads.Interpreter/Exceptions/InvalidOperatorException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Threads.Interpreter.Exceptions {
+    /// <summary>
+    /// Thrown when an expression contains invalid operator use.
+    /// </summary>
+    public class InvalidOperatorException : Exception {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidOperatorException" /> class.
+        /// </summary>
+        public InvalidOperatorException() {}
+    }
+}
